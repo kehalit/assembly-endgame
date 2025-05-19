991c14db328d050c28ac1bf08deac59640b587f7
Kehalit <88938730+kehalit@users.noreply.github.com>
2025-05-19 15:33:40 +0200

    Added game UI layout, language chips, word display, keyboard logic, and endgame status handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,121 @@
 import React from "react"
-import {languages} from './languages'
-import {useState} from 'react'
-
+import { languages } from './languages'
+import { useState } from 'react'
+import clsx from "clsx"
 
 
 export default function AssemblyEndgame() {
 
-  const [currentWord, setCurrentWord]= useState("react")
+  //state values
+  const [currentWord, setCurrentWord] = useState("react")
+  const [guessedLetters, setGuessedLetters] = useState([])
+
+  // Derived values
+  const wrongGuessCount = 
+    guessedLetters.filter(letter => !currentWord.includes(letter)).length
+
+    const isGameWon = currentWord.split("").every(letter => guessedLetters.includes(letter))
+    const isGameLost = wrongGuessCount >= languages.length -1 
+    const isGameOver = isGameWon || isGameLost
+  
+//static values
   const alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+  function addGuessedLetter(letter) {
+    setGuessedLetters(prevLetters =>
+      prevLetters.includes(letter) ?
+        prevLetters : 
+          [...prevLetters, letter])
+  }
 
-  const languageElements = languages.map(lang => {
+
+  const languageElements = languages.map((lang, index) => {
+   
+    
+    const isLanguageLost = index < wrongGuessCount
     const styles = {
-      backgroundColor:lang.backgroundColor,
-      color:lang.color
+        backgroundColor: lang.backgroundColor,
+        color: lang.color
     }
-  return (
-    <span 
-     className="chip"
-     style={styles}
-     key={lang.name}
-    >
-      {lang.name}
-    </span>
-    )
-  } 
-)
-  const letterElements = currentWord.split("").map((letter, index) => (
-    <span key={index}>{letter.toUpperCase()}</span>
-  )
-  )
-
-  const keyboardElement = alphabet.split("").map(char => (
-      <button 
-      key = {char} >
-      {char.toUpperCase()}
-      </button>
-  ))
     return (
-
-        <main>
-          <header>
-            <h1>Assembly: Endgame</h1>
-            <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
-          </header>
-
-          <section className = "game-status">
-            <h2> You Win!</h2>
-            <p>Well done! 🎉</p>
-          </section>
-          <section className="language-chips">
-              {languageElements}
-
-          </section>
-         
-          <section className="word"> 
-            {letterElements}
-            </section>
-          <section className="keyboard">{keyboardElement}</section>
-          <button className="new-game">New Game</button>
-        </main>
+        <span
+            className={`chip ${isLanguageLost ? "lost" : ""}`}
+            //className ="chip"
+            style={styles}
+            key={lang.name}
+        >
+            {lang.name}
+        </span>
     )
+})
+
+
+  const letterElements = currentWord.split("").map((letter, index) => (
+    <span key={index}>{guessedLetters.includes(letter) ? letter.toUpperCase() : ""}</span>
+  )
+  )
+
+  const keyboardElements = alphabet.split("").map(char => {
+    const isGuessed = guessedLetters.includes(char)
+    const isCorrect = isGuessed && currentWord.includes(char)
+    const isWrong = isGuessed && !currentWord.includes(char)
+    const className = clsx({
+        correct: isCorrect,
+        wrong: isWrong
+    })
+
+    
+    return (
+        <button
+            className={className}
+            key={char}
+            onClick={() => addGuessedLetter(char)}
+        >
+            {char.toUpperCase()}
+        </button>
+    )
+})
+
+const gameStatusClass = clsx("game-status", {
+  won: isGameWon,
+  lost: isGameLost
+})
+  return (
+
+    <main>
+      <header>
+        <h1>Assembly: Endgame</h1>
+        <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+      </header>
+
+      <section className={gameStatusClass}>
+                {isGameOver ? (
+                    isGameWon ? (
+                        <>
+                            <h2>You win!</h2>
+                            <p>Well done! 🎉</p>
+                        </>
+                    ) : (
+                        <>
+                            <h2>Game over!</h2>
+                            <p>You lose! Better start learning Assembly 😭</p>
+                        </>
+                    )
+                ) : (
+                        null
+                    )
+                }
+            </section>
+      <section className="language-chips">
+        {languageElements}
+
+      </section>
+
+      <section className="word">
+        {letterElements}
+      </section>
+      <section className="keyboard">{keyboardElements}</section>
+      {isGameOver && <button className="new-game">New Game</button>}
+    </main>
+  )
 }
