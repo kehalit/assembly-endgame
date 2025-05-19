a917da045349d88763b6c9b505cda1d5ec1b2aac
Kehalit <88938730+kehalit@users.noreply.github.com>
2025-05-19 10:51:28 +0200

    header section and game status section is added and styled

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React from "react"
+import {languages} from './languages'
+
+
+
+export default function AssemblyEndgame() {
+  const languageElements = languages.map(lang => {
+    const styles = {
+      backgroundColor:lang.backgroundColor,
+      color:lang.color
+    }
+  return (
+    <span 
+     className="chip"
+     style={styles}
+     key={lang.name}
+    >
+      {lang.name}
+    </span>
+    )
+  } 
+)
+
+    return (
+
+        <main>
+          <header>
+            <h1>Assembly: Endgame</h1>
+            <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+          </header>
+
+          <section className = "game-status">
+            <h2> You Win!</h2>
+            <p>Well done! 🎉</p>
+          </section>
+          <section className="language-chips">
+              {languageElements}
+
+          </section>
+         
+            
+        </main>
+    )
+}
